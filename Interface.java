ffce484bffa49d2fd8d32980260f9796650bc2e2
tjohnson433 <tjohnson433@icloud.com>
2025-12-01 15:39:08 +0000

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,5 +1,4 @@
 package ticTacToe;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -14,19 +13,20 @@ public class Interface extends JPanel implements ActionListener{
 	private static JPanel panelswitcher = new JPanel(cardlayout);
 	private static JPanel winPanel = new JPanel();
 	private static JLabel winLabel = new JLabel();
-	
 	private static JButton resetButton  =new JButton("Reset Game");
-		
+
+	private String theWinner = " ";
+	
+	
 		//constructor
 	public Interface() {
 		
 		// setting up the win panel
 		
-		
-		winLabel.setText("YOU WINNNNNNNNNNNN");
-		winLabel.setForeground(Color.black);
-		winPanel.setBackground(Color.white);
-		winPanel.add(winLabel);
+		winPanel.setLayout(new BorderLayout());
+		winLabel.setFont(new Font("Jumble", Font.BOLD, 20));
+		winLabel.setHorizontalAlignment(JLabel.CENTER);
+		winPanel.add(winLabel, BorderLayout.CENTER);
 			
 		panelswitcher.add(myGamePanel, "Gamepanel");
 		panelswitcher.add(winPanel, "Win Panel");
@@ -39,18 +39,32 @@ public class Interface extends JPanel implements ActionListener{
 	}
 	
 	
+	public void showWhoWon() {
+		
+		if (myGamePanel.getWinner() == 0) {
+			theWinner = "Draw";
+			winLabel.setText("Draw");
+		} else if (myGamePanel.getWinner() == 1) {
+			theWinner = "Crosses wins";
+			winLabel.setText("Crosses wins");
+		} else {
+			theWinner = "Noughts wins";
+			winLabel.setText("Noughts wins");
+		}
+		
+	}
 	
 	public static void showWinPanel() {
 		cardlayout.show(panelswitcher, "Win Panel");
-		System.out.println("showWinPanel called");
 	}
-	
-	
 	
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+
+		myTurnLabel.setHorizontalAlignment(JLabel.CENTER);
 		myTurnLabel.setText("Turn: " + myGamePanel.getTurn());
+		showWhoWon();
 	}
 }
