0888c5cd3aae2c6e5ddf0b32a2ae4fb102b2a026
tjohnson433 <tjohnson433@icloud.com>
2025-12-12 11:52:25 +0000

    Refactor Interface class to include menu options

--- Interface.java
+++ Interface.java
@@ -7,18 +7,21 @@ import javax.swing.*;
 public class Interface extends JPanel implements ActionListener{
 	
 	//attributes
+	
 	private static JLabel myTurnLabel = new JLabel("Turn: X");
 	private static GamePanel myGamePanel = new GamePanel();                              // screen1
 		
-	private static CardLayout cardlayout = new CardLayout();
-	private static JPanel panelswitcher = new JPanel(cardlayout);
-	private static JPanel winPanel = new JPanel();
+	private static CardLayout cardlayout = new CardLayout();                  // creates the cardLayout
+	private static JPanel panelswitcher = new JPanel(cardlayout);             // tells the JPanel how to arrange the contents
+	private static JPanel winPanel = new JPanel();                            // screen2
 	private static JLabel winLabel = new JLabel();
 	private static JPanel buttonsPanel = new JPanel();
-	private static JPanel myTurnPanel = new JPanel();
+	private static JPanel menuPanel = new JPanel();
+	private static JButton menuButton = new JButton("Main Menu");
+	private static JButton ticTacToeButton = new JButton();
+	private static JButton connectFourButton = new JButton();
 	private static JButton resetButton  = new JButton("Reset Game");
-
-	private String theWinner = " ";
+	private static String theWinner = " ";
 	
 	
 		//constructor
@@ -26,35 +29,62 @@ public class Interface extends JPanel implements ActionListener{
 		
 		// setting up the win panel
 		
-		winPanel.setLayout(new BorderLayout());
-		winPanel.setBackground(Color.WHITE);
+
 		winLabel.setFont(new Font("Jumble", Font.BOLD, 20));
 		winLabel.setHorizontalAlignment(JLabel.CENTER);
+		
+		winPanel.setLayout(new BorderLayout());
+		winPanel.setBackground(Color.WHITE);
 		winPanel.add(winLabel, BorderLayout.CENTER);
+		winPanel.add(buttonsPanel, BorderLayout.SOUTH);
+		winPanel.add(myTurnLabel, BorderLayout.NORTH);
 			
 		panelswitcher.add(myGamePanel, "Gamepanel");
 		panelswitcher.add(winPanel, "Win Panel");
+		panelswitcher.add(menuPanel, "Menu Panel");
 
 		this.setLayout(new BorderLayout());
 		this.add(panelswitcher, BorderLayout.CENTER);
 		this.add(myTurnLabel, BorderLayout.NORTH);
 		
-		myTurnPanel.add(myTurnLabel, BorderLayout.NORTH);
-		myTurnLabel.setHorizontalAlignment(JLabel.CENTER);
+		myTurnLabel.setHorizontalAlignment(JLabel.CENTER);	
 		myTurnLabel.setBackground(Color.WHITE);
 		
 		buttonsPanel = new JPanel();
-		buttonsPanel.setLayout(new GridLayout(1, 4));
+		buttonsPanel.setLayout(new GridLayout(1, 2));
 		buttonsPanel.add(resetButton);
+		buttonsPanel.add(menuButton);
 		
 		resetButton.addActionListener(this);
 		resetButton.setText("Play Again");
 		resetButton.setFont(new Font("Jumble", Font.BOLD, 20));
 		resetButton.setBackground(Color.WHITE);
 		resetButton.setFocusable(false);
-
-		winPanel.add(buttonsPanel, BorderLayout.SOUTH);
-		winPanel.add(myTurnPanel, BorderLayout.NORTH);
+		
+		ticTacToeButton.addActionListener(this);
+		ticTacToeButton.setText("Tic Tac Toe");
+		ticTacToeButton.setFont(new Font("Jumble", Font.BOLD, 20));
+		ticTacToeButton.setBackground(Color.WHITE);
+		ticTacToeButton.setFocusable(false);
+		
+		connectFourButton.addActionListener(this);
+		connectFourButton.setText("Connect 4");
+		connectFourButton.setFont(new Font("Jumble", Font.BOLD, 20));
+		connectFourButton.setBackground(Color.WHITE);
+		connectFourButton.setFocusable(false);
+		
+		menuButton.addActionListener(this);
+		menuButton.setText("Main Menu");
+		menuButton.setFont(new Font("Jumble", Font.BOLD, 20));
+		menuButton.setBackground(Color.WHITE);
+		menuButton.setFocusable(false);
+		
+		
+		
+		menuPanel.setLayout(new BorderLayout());
+		menuPanel.setBackground(Color.WHITE);
+		menuPanel.add(connectFourButton, BorderLayout.EAST);
+		menuPanel.add(ticTacToeButton, BorderLayout.WEST);
 		
 		
 		myGamePanel.setActionListener(this);
@@ -80,12 +110,15 @@ public class Interface extends JPanel implements ActionListener{
 		cardlayout.show(panelswitcher, "Win Panel");
 	}
 	
+	public static void showMenuPanel() {
+		cardlayout.show(panelswitcher, "Menu Panel");
+	}
 	
-//	public void resetBoard() {
-//		for (int i = 0; i < 9; i++) { 
-//			GamePanel
-//		}
-//	}
+	public void resetBoard() {
+		myGamePanel.resetBoard();
+		myTurnLabel.setText("Turn: " + myGamePanel.getTurn());
+		cardlayout.show(panelswitcher, "Gamepanel");
+	}
 	
 	
 	@Override
@@ -94,9 +127,15 @@ public class Interface extends JPanel implements ActionListener{
 
 		myTurnLabel.setText("Turn: " + myGamePanel.getTurn());
 		showWhoWon();
-		
-//		if (e.getSource() == resetButton) {
-//			resetBoard();
-//		}
+		if (e.getSource() == resetButton) { 
+			resetBoard();
+		}
+		if (e.getSource() == menuButton) {
+			showMenuPanel();
+		}
 	}
 }
+
+
+
+
